1df3eabe149c2ba7493925f32b54a7d203c5984a
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-12 23:08:05 -0700

    completed reverse 02

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,1 +1,10 @@
 // Enter your code here
+function funcvalidate(){
+  var number = document.getElementById("input").value;
+  if (isNaN(number) || number.toString().length < 8) {
+    text = "Please enter 8 digit number to proceed.";
+  } else {
+    text = number.toString().split("").reverse().join("");
+  }
+  alert(text);  
+}

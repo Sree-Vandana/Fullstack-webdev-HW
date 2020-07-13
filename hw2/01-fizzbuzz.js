43e1616065995c7b67924426efd571910761c59d
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-12 22:01:56 -0700

    completed fizzbuzz

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -1,1 +1,15 @@
 // Enter your code here
+var num = "";
+for(var i = 1; i<=100; i++ ){
+  if(i%5 == 0 & !i%3 == 0)
+  num += "buzz" + "<br>";
+  else if(i%3 == 0 & !i%5 == 0)
+  num += "fizz" + "<br>";
+  else
+  num += i + "<br>";
+  if(i%3 == 0 & i%5 == 0)
+  num += "fizzbuzz" + "<br>";
+}
+document.getElementById("results").innerHTML = num;
+
+

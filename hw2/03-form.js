837a8a66edf47ef648d9b4b9d9e0669ff794ee2a
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-14 22:26:07 -0700

    completed 03forms and 04events

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,1 +1,14 @@
 // Enter your code here
+function displayOnConsole(){
+  //console.log("Hello World");
+  console.log('name: ' + document.getElementById('name').value);
+  console.log('email: ' + document.getElementById('email').value);
+  console.log('comments: ' + document.getElementById('Submityourmessage').value);
+  let checkbox = document.getElementById('newsletter');
+  let cb = 'on';
+  if(!checkbox.checked){
+    cb = 'off';
+  }
+  console.log('newsletter: ' + cb);
+  return false;
+}
--- hw2/04-events.js
+++ hw2/04-events.js
@@ -1,1 +1,10 @@
 // Enter your code here
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+
+function displayWindowSize() {
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+  document.getElementById('width').innerHTML = w;
+  document.getElementById('height').innerHTML = h;
+};

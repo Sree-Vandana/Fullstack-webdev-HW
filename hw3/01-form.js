0d76207763e355d16321c77e38c0d7550c22fd15
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-24 07:17:24 -0700

    1st ex completed

--- hw3/01-form.js
+++ hw3/01-form.js
@@ -1,1 +1,35 @@
 // Enter your code here
+var express = require('express');
+var app = express();
+const port = 3000;
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + "/" + 'index.html');
+});
+console.log("index.html");
+
+app.post('/submit', (req, res)=>{
+  var newsletter = req.body.newsletter;
+  var comments = req.body.Submityourmessage;
+  if(comments.length==0){
+    comments = "n/a";
+  }
+  if(newsletter == "newsletter"){
+    newsletter = "Yes, I would like to sign up for the newsletter.";
+  }
+  else{
+    newsletter = "No, Thank you.";
+  }
+
+  res.write("Name: " + req.body.name + "\n");
+  res.write("Email: " + req.body.email + "\n");
+  res.write("comments: " + comments + "\n");
+  res.write("Newsletter: " + newsletter + "\n");
+  res.end();
+})
+
+app.listen(port, ()=>{
+  console.log(`Server running at http://localhost:${port}`);
+})
